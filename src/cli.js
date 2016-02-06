97f7ecb6ba44db3636aa6f1fbc7d4ade3401e1af
Justin Ball <justinball@gmail.com>
2016-02-05 18:17:53 -0700

    Added notes

--- src/cli.js
+++ src/cli.js
@@ -15,15 +15,21 @@ if(!program.args.length) {
     case "create":
       if(args[1] == "instance"){
         // Create an instance
-        
+        // Add instance json ie usu-dev, usu-prod, etc
       } else {
         // Create an app
-
+        // Clone github repo
+        // Change values
+        // Add initial instances - dev, prod, staging
       }
       break;
     case "deploy":
+      // Call api install, pass all instance json files.
+      // Get back token for uploading code
+      // Will need to execute build in current directory, get build files and upload them
       break;
     case "install":
+      // Perform LTI install
       break;
   }
 }
